c361c5fc3d04f3e643f07d07f338d0b0d9ce2220
Alex Paños <alejandro.panos@sogiteck.net>
2017-03-20 17:03:51 +0100

    GET, POST, PUT y DELETE en el api de prueba de coches

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,77 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\Coche;
 
-class DefaultController extends Controller
-{
+class DefaultController extends FOSRestController {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+                    'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ]);
+    }
+
+    /**
+     * @Rest\Get("/api")
+     */
+    public function getAction() {
+        $restresult = $this->getDoctrine()->getRepository('AppBundle:Coche')->findAll();
+        if ($restresult === null) {
+            return new View("No hay coches en la bbdd", Response::HTTP_NOT_FOUND);
+        }
+        return $restresult;
+    }
+
+    /**
+     * @Rest\Post("/api")
+     */
+    public function postAction(Request $request) {
+        $coche = new Coche;
+        $marca = $request->get('marca');
+        $coche->setMarca($marca);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($coche);
+        $em->flush();
+        return new View("Coche añadido correctamente", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put("/api/{id}")
+     */
+    public function updateAction($id, Request $request) {
+        $marca = $request->get('marca');
+        $sn = $this->getDoctrine()->getManager();
+        $cocheId = $this->getDoctrine()->getRepository('AppBundle:Coche')->find($id);
+        if (empty($cocheId)) {
+            return new View("Coche no encontrado", Response::HTTP_NOT_FOUND);
+        } elseif (!empty($marca)) {
+            $cocheId->setMarca($marca);
+            $sn->flush();
+            return new View("Marca de coche actualizada correctamente", Response::HTTP_OK);
+        }
+    }
+
+    /**
+     * @Rest\Delete("/api/{id}")
+     */
+    public function deleteAction($id) {
+        $sn = $this->getDoctrine()->getManager();
+        $coche = $this->getDoctrine()->getRepository('AppBundle:Coche')->find($id);
+        if (empty($coche)) {
+            return new View("Marca de coche no encontrada", Response::HTTP_NOT_FOUND);
+        } else {
+            $sn->remove($coche);
+            $sn->flush();
+        }
+        return new View("Marca borrada correctamente", Response::HTTP_OK);
     }
 }
---
+++ src/AppBundle/Entity/Coche.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Coche
+ *
+ * @ORM\Table(name="coche")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CocheRepository")
+ */
+class Coche
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="marca", type="string", length=255)
+     */
+    private $marca;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set marca
+     *
+     * @param string $marca
+     *
+     * @return Coche
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get marca
+     *
+     * @return string
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+}
+
